9bc2f37f9576532653bd0712ba125db54bff9976
Shild <FraternaliAlessandro@gmail.com>
2020-09-15 15:17:07 +0200

    +

---
+++ 7classi/app.js
@@ -0,0 +1,41 @@
+/* jshint esnext:true */
+
+class Alien{
+  constructor(weapons, ranks){
+    this.weapons = weapons || ['Plasma Pistol'];
+    this.shield = 100;
+    this.ranks = ranks || ['Rank: '];
+  }
+  getWeapons(){
+    return this.weapons;
+  }
+
+  getRanks(){
+    return this.ranks;
+  }
+
+  increaseShield(shield){
+    this.shield += shield;
+  }
+  getShield(){
+    return this.shield;
+  }
+};
+
+var alien = new Alien(['Plasma Pistol', 'Plasma Rifle']);
+// var alien = new Alien(['Rank: ']);
+
+Alien.prototype.addWeapon = function(weapon){
+  this.weapons.push(weapon);
+}
+
+Alien.prototype.addRank = function(rank){
+  this.ranks.push(rank);
+}
+
+alien.addRank('Zealot'),
+alien.addWeapon('Energy Sword'),
+alien.increaseShield(150);
+console.log(alien.getRanks());
+console.log(alien.getShield());
+console.log(alien.getWeapons());
